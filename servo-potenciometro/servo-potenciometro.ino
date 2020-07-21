4de5185d18c889326529cfbbfd9751d4b4adc0f1
Bruno Serrão <brunosserrao@gmail.com>
2020-07-21 22:10:17 +0100

    potenciometer and led

--- servo-potenciometro/servo-potenciometro.ino
+++ servo-potenciometro/servo-potenciometro.ino
@@ -1,21 +1,26 @@
 #include <Servo.h>
 const int analogPort = 0;
+const int digitalPort = 7;
 
 Servo servo;
 
 void setup(){
   servo.attach(2);
+  pinMode(digitalPort, OUTPUT); 
+  
   Serial.begin(9600);
 }
 
 void loop(){
   int val = analogRead(analogPort);
-
   int angle = val / 5.7;
+  int bright = val/4;
+
   servo.write(angle);
+  analogWrite(digitalPort, bright); 
 
-  // Serial.println(val);
+
+  Serial.println(bright);
   // Serial.println(angle);
-
   // delay(100);
 }
